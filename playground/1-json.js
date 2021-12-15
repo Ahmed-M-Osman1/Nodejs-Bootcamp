0cd9423908dac3552168ef80cdcdc10b56c30830
akiid777 <akiid777@gmail.com>
2021-12-15 17:23:06 +0200

    munipilating json files

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,1 +1,22 @@
-aa
+const fs = require('fs')
+// const book = {
+//     title:'Ego is the Enemy',
+//     author: 'Ryan Holiday'
+// }
+
+// const bookJSON = JSON.stringify(book)
+// fs.writeFileSync('1-json.json',bookJSON)
+
+// const dateBuffer = fs.readFileSync('1-json.json')
+// const dataJSON = dateBuffer.toString()
+// const data = JSON.parse(dataJSON)
+// console.log(data)
+
+
+fs.readFileSync('1-json.json')
+const dateBuffer = fs.readFileSync('1-json.json')
+const dataJSON = dateBuffer.toString()
+const data = JSON.parse(dataJSON)
+data.name = "Ahmed",
+data.age = 28
+console.log(data)
